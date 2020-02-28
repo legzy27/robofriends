7db12cbcb287a082f2d5c42e92e993ac45308755
Alex Oyedele <niyo4real2002@yahoo.comm>
2020-02-28 11:32:39 +0000

    updated new gitignore

---
+++ build/precache-manifest.8ee0d271bcedd30f9319cd3ef367a7c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fab5bd620d6f964c4e8d1e9dbdd3f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f65b480f3ae2950728cf",
+    "url": "/static/css/main.9a6dc019.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65b480f3ae2950728cf",
+    "url": "/static/js/main.dd515a18.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
